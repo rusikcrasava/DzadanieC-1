451ec79ff935199ef989e77494e38f98545f3334
rusikcrasava <rusikcrasava@mail.ru>
2022-12-16 13:06:06 +0300

    Написан и проверен код к 3 задаче

---
+++ zada4a3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int Chetnechet = Convert.ToInt32(Console.ReadLine());
+if (Chetnechet%2 == 0)
+Console.WriteLine("Ваше число, является четным!");
+else
+Console.WriteLine("Ваше число, является НЕчетным!");
+
