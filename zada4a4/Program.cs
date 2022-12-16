0e485a2f92fa6eeb27ff1febe309a1dbe110df5f
rusikcrasava <rusikcrasava@mail.ru>
2022-12-16 13:49:10 +0300

    Написан и проверен код к 4 задаче

---
+++ zada4a4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число:");
+int Vsechetnye = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (i < Vsechetnye)
+{
+    i += 2;
+    if (i <= Vsechetnye)
+    Console.WriteLine(i);
+}
+Console.WriteLine("Все числа лежащие в данном диапазоне были проверены.");
