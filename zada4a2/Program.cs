aefc92d4ce33ba1170682acc6f72c904a43ee3f6
rusikcrasava <rusikcrasava@mail.ru>
2022-12-16 13:01:11 +0300

    Написан и проверен код ко 2 задаче

---
+++ zada4a2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число:");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+if (Number1 > Number2) 
+{ 
+    if (Number1 > Number3) 
+    { 
+        Console.WriteLine($"Максимальное значение = {Number1} "); 
+    } 
+ 
+} 
+else if (Number3 > Number2) 
+{ 
+    Console.WriteLine($"Максимальное значение = {Number3} "); 
+} 
+ 
+else 
+{ 
+    Console.WriteLine($"Максимальное значение = {Number2} "); 
+}
