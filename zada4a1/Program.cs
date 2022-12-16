fadc6e8cd0628e12431cd67fbed40b936894bf55
rusikcrasava <rusikcrasava@mail.ru>
2022-12-16 12:49:12 +0300

    Написан и проверен код к 1 задаче

---
+++ zada4a1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число:");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+if (Num1 < Num2)
+{
+    Console.WriteLine($"Максимальное значение = {Num2} , Минимальное значение = {Num1} ");
+}
+else
+{
+    Console.WriteLine($"Минимальное значение = {Num1} , Максимальное значение = {Num2} ");
+}
